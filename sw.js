6b898486c019aa643f2fb9b0689ecff0c576f940
mmsndvl <santosidadmm@gmail.com>
2026-07-28 04:25:28 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Service worker: caches the app so it works offline after the first visit
+var CACHE = "my-todo-tracker-v1";
+var FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(function (r) {
+      return r || fetch(e.request);
+    })
+  );
+});
